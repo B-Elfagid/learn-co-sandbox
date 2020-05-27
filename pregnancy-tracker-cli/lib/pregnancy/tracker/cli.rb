24ebed28fa172bbac9d8c925d9224fe8aad86f48
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-05-27 20:29:37 +0000

    modified README

---
+++ pregnancy-tracker-cli/lib/pregnancy/tracker/cli.rb
@@ -0,0 +1,10 @@
+require "pregnancy/tracker/cli/version"
+
+module Pregnancy
+  module Tracker
+    module Cli
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ pregnancy-tracker-cli/lib/pregnancy/tracker/cli/version.rb
@@ -0,0 +1,7 @@
+module Pregnancy
+  module Tracker
+    module Cli
+      VERSION = "0.1.0"
+    end
+  end
+end

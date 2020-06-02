5d868f61f8cf9924c5db935d36cefb8edcc9da33
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-06-02 17:28:17 +0000

    modified bin

--- beer_cli/lib/beer_cli.rb
+++ beer_cli/lib/beer_cli.rb
@@ -1,6 +1,18 @@
-require "beer_cli/version"
 
-module BeerCli
-  class Error < StandardError; end
+#appilication environment 
+
+require 'bundler/setup'
+Bundler.require(:default)
+
+require_relative './beer_cli/cli.rb'
+require_relative './beer_cli/api.rb'
+require_relative './beer_cli/beer.rb'
+
+
+
+#require "beer_cli/version"
+
+#module BeerCli
+  #class Error < StandardError; end
   # Your code goes here...
-end
+#end
---
+++ beer_cli/lib/beer_cli/beer.rb
@@ -0,0 +1,3 @@
+class Beer
+  puts "Beer class loaded"
+end 

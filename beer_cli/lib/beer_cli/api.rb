3defbe3e70ce415a857b801013dd6cc5c5000c6f
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-06-02 23:25:07 +0000

    WIP: modify lib

--- beer_cli/lib/beer_cli/api.rb
+++ beer_cli/lib/beer_cli/api.rb
@@ -1,3 +1,9 @@
 class Api 
-  puts "Api class loaded"
+  
+ BASE_URL = 'https://api.punkapi.com/v2/beers/?limit=10'
+ def self.get_beers
+   res = RestClient.get(BASE_URL)
+   binding.pry
+ end 
+   
 end 

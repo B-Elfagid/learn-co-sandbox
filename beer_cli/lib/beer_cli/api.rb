df6d84cb7921f9a4769a9d0739383a0380189181
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-06-03 00:54:08 +0000

    WIP: Adding initial file/folder structure

--- beer_cli/lib/beer_cli/api.rb
+++ beer_cli/lib/beer_cli/api.rb
@@ -3,7 +3,11 @@ class Api
  BASE_URL = 'https://api.punkapi.com/v2/beers/?limit=10'
  def self.get_beers
    res = RestClient.get(BASE_URL)
-   binding.pry
- end 
+   data = JSON.parse(res.body)
+   data.each do |beers|
+  puts beers["name"]
+  puts beers["image_url"]
+  end
+end 
    
 end 

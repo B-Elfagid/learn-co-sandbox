7e93ca5281566d6ebf0b92f0df69905ff195b121
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-06-04 18:10:33 +0000

    WIP: Adding initial file/folder structure

--- beer_cli/lib/beer_cli/cli.rb
+++ beer_cli/lib/beer_cli/cli.rb
@@ -1,22 +1,46 @@
 class Cli
   
   def run 
-    welcome 
+    print_welcome 
     Api.get_beers
     main
   end 
   
-  def welcome
-    puts "Welcome to explore about beer types!"
+  def print_welcome
+    puts "Welcome to explore about Beer types!"
   
   end 
   
   def main 
     print_all
+    print_selection_prompt
+    id = prompt_selection
+    
   end
   
   def print_all
     Beer.all.each {|b| puts "#{b.id}.) #{b.name}"}
   end 
   
+  def print_selection_prompt
+    puts "Please select a number of your favourite Beer for more information"
+  end 
+  
+  def prompt_selection
+    gets.chomp
+end 
+
+def valid_id?(id)
+  id = id.to_i
+  if id < 1 || id > Beer.all.size
+    print_error
+    main 
+end 
+id
+end 
+
+def print_error 
+  puts "Invalid Selection, please try again."
+end 
+
 end 

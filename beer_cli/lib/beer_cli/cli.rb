84ed71e2782f940e3ae90d7039670e0b3832de79
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-06-04 17:27:15 +0000

    WIP: Adding initial file/folder structure

--- beer_cli/lib/beer_cli/cli.rb
+++ beer_cli/lib/beer_cli/cli.rb
@@ -1,3 +1,22 @@
 class Cli
-  puts "Cli class loaded"
+  
+  def run 
+    welcome 
+    Api.get_beers
+    main
+  end 
+  
+  def welcome
+    puts "Welcome to explore about beer types!"
+  
+  end 
+  
+  def main 
+    print_all
+  end
+  
+  def print_all
+    Beer.all.each {|b| puts "#{b.id}.) #{b.name}"}
+  end 
+  
 end 

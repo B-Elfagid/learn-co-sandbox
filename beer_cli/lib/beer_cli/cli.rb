102695aa9b545c216768b18f5e3c8dfe2047af49
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-06-04 23:43:17 +0000

    modify lib

--- beer_cli/lib/beer_cli/cli.rb
+++ beer_cli/lib/beer_cli/cli.rb
@@ -1,4 +1,44 @@
 class Cli
-  puts "hi"
- 
+  
+  def run 
+    print_welcome
+    Api.get_beers
+    main 
+  end 
+  
+  def main
+    print_all
+    print_selection_prompt
+   id = valid_id?(prompt_selection)
+  end 
+  
+  def print_all
+    Beer.all.each {|b| puts "#{b.id}.) #{b.name}" }
+  end 
+  
+  def print_selection_prompt
+    puts "Please select Beer by number for more information"
+  end
+  
+   def print_welcome 
+    puts "Welcome to explore about Beer types!"
+  end 
+  
+  def print_error
+    puts "Invalid Selection, please try again"
+  end 
+  
+  def prompt_selection 
+    gets.chomp
+  end 
+  
+  def valid_id?(id)
+   id = id.to_i
+  if id < 1 || id > Beer.all.size
+    print_error
+    sleep 1
+   main 
+end 
+id
+end 
 end 

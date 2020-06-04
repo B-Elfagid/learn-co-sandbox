777d0fa946be8a095f230f3659defd7046918a69
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-06-04 23:12:29 +0000

    modify lib

--- beer_cli/lib/beer_cli/api.rb
+++ beer_cli/lib/beer_cli/api.rb
@@ -1,20 +1,13 @@
 class Api 
-  
- BASE_URL = 'https://api.punkapi.com/v2/beers/?limit=10'
- def self.get_beers
-   res = RestClient.get(BASE_URL)
-   data = JSON.parse(res.body)
-   data.each do |beers|
-  name = beers["name"]
-  id = beers["id"]
-  Beer.new(name, id)
-  end
-end 
-
-def self.get_details_by_id(id)
-res = RestClient.get("#{BASE_URL} #{id}")
-data = JSON.parse(res.body)
-binding.pry
-  
+ 
+BASE_URL = "https://api.punkapi.com/v2/beers/"
+def self.get_beers
+  res = RestClient.get(BASE_URL)
+  data = JSON.parse(res.body)
+  data.each do |beers|
+    name = beers["name"]
+    id = beers["id"]
+    Beer.new(name, id)
+  end 
 end 
 end 
--- beer_cli/lib/beer_cli/beer.rb
+++ beer_cli/lib/beer_cli/beer.rb
@@ -1,17 +1,14 @@
 class Beer
-  attr_reader :name, :id
-  
-  @@all = []
-  
-  def initialize(name, id)
-    @name = name
-    @id = id
-    @@all << self
-  end 
-  
-  def self.all
-    @@all
-    
-end 
+ attr_reader :name, :id 
+ @@all = []
+ def initialize(name, id)
+   @name = name
+   @id = id 
+   @@all << self 
+ end 
+ 
+ def self.all
+   @@all
+ end 
 
 end 
